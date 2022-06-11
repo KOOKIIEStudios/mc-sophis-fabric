68ffbf8db0af12964f77de2d19ece813c57041c9
HemyO <hemyototem@gmail.com>
2022-06-11 22:16:28 +0800

    Configure completely

---
+++ src/main/java/hm/o/sph/Sophis.java
@@ -0,0 +1,20 @@
+package hm.o.sph;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sophis implements ModInitializer {
+
+    public static final String MODID = "sph";
+
+    public static final FabricLoader FABRIC = FabricLoader.getInstance();
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID.toUpperCase());
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
+}
